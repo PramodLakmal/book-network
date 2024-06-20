a5274c6d1566d554f05a8c97f633e82e9291654f
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-06-20 13:50:41 +0530

    initial commit

---
+++ src/test/java/com/pramod/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramod.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
