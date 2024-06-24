14bb4e67de339e9cdb8ded675eb005606e8c100c
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-06-24 10:38:30 +0530

    implement the user details service

---
+++ src/main/java/com/pramod/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.pramod.book.security;
+
+import com.pramod.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + userEmail));
+    }
+}
