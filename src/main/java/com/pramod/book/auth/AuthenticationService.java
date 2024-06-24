52d0c7375d0ffc044e74590549eaf7bf829f88c8
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-06-24 12:16:58 +0530

    create the authentication controller

---
+++ src/main/java/com/pramod/book/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.pramod.book.auth;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(@Valid RegistrationRequest request) {
+    }
+}
