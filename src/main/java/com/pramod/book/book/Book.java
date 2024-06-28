b985cfe42833801fc6acfdbc6684f17a8d0b7b27
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-06-28 13:23:56 +0530

    implement the find book by id

--- src/main/java/com/pramod/book/book/Book.java
+++ src/main/java/com/pramod/book/book/Book.java
@@ -40,4 +40,16 @@ public class Book extends BaseEntity {
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
 
+    @Transient
+    public double getRate() {
+        if (feedbacks == null || feedbacks.isEmpty()) {
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundedRate = Math.round(rate * 100.0) / 100.0;
+        return roundedRate;
+
 }
--- src/main/java/com/pramod/book/book/BookMapper.java
+++ src/main/java/com/pramod/book/book/BookMapper.java
@@ -14,4 +14,18 @@ public class BookMapper {
                 .shareable(request.shareable())
                 .build();
     }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsis(book.getSynopsis())
+                .owner(book.getOwner().fullName())
+                .rate(book.getRate())
+                .shareable(book.isShareable())
+                .archived(book.isArchived())
+                .build();
+    }
 }
