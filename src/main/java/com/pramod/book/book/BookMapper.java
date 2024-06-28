ede815d0b5f82cca72f5aa2e8289e647678fb550
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-06-28 13:09:48 +0530

    implement the save book method

---
+++ src/main/java/com/pramod/book/book/BookMapper.java
@@ -0,0 +1,17 @@
+package com.pramod.book.book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .authorName(request.authorName())
+                .isbn(request.isbn())
+                .synopsis(request.synopsis())
+                .shareable(request.shareable())
+                .build();
+    }
+}
